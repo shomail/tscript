fd21fe3e91870c070a66688bab3290785ec6a69a
Shomail <shomail1988@gmail.com>
2018-12-04 14:15:32 +0100

    enum and tuples excercise

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,64 @@ function shuffleArray(a: any[]) {
   }
 }
 
-// export class Dealer {
+export enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades
+}
 
-// }
+export enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+}
+
+type Card = [Suit, CardNumber];
+
+let c: Card = [Suit.Clubs, CardNumber.Ace];
+
+function createDeck(): Card[] {
+  let cards: Card[] = [];
+
+  for (let s = 0; s < Object.keys(Suit).length; s += 2) {
+    for (let n = 0; n < Object.keys(CardNumber).length; n += 2) {
+      cards.push([s / 2, n / 2]);
+    }
+  }
+  return cards;
+}
+
+export class Dealer {
+  cards: Card[] = [];
+
+  constructor() {
+    this.cards = createDeck();
+    shuffleArray(this.cards);
+  }
+
+  dealHand(numCards: number): Card[] {
+    if (numCards > this.getLength()) throw new Error('Not enough cards!');
+    if (numCards < 0) throw new Error('Please give me your Cards!');
+    return this.cards.splice(this.getLength() - numCards, numCards);
+  }
+
+  getLength(): number {
+    return this.cards.length;
+  }
+
+  readCard(card: Card): string {
+    let [suit, cardNumber] = card;
+    return `${CardNumber[cardNumber]} of ${Suit[suit]}`;
+  }
+}
---
+++ notes.md
@@ -0,0 +1,21 @@
+--Classes--
+
+Javascript uses prototype for inheritance, but classes provides better syntax.
+
+Inside classes constructor function is used to create new object.
+
+Methods can be defined in a similar way as on objects, but unlike objects there is no commas between them.
+
+Static methods are attched to the contructor or the class itself, insetead of instance.
+
+Subclasses can be created by using 'extends' keyword.
+
+The Super keword can be used to call methods on the parent class.
+
+--Mixins--
+
+Mixins are like abstract classes or template for classes.
+
+--Enums--
+
+Are TS specific, basically a list of specific values.
