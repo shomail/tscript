7b7651698a8817c52fb3d8526b6b9b19834f0d06
Shomail <shomail1988@gmail.com>
2018-12-05 13:29:41 +0100

    function and return type with interface

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,33 @@
-// export function cashier() {
-//   return {};
-// }
+interface CartItem {
+  name: string;
+  price: number;
+  qty: number;
+}
+
+interface CartAPI {
+  length: number;
+  total: number;
+  addItem(item: CartItem): CartAPI;
+  add(name: string, price: number, qty: number): CartAPI;
+}
+
+export function cashier(): CartAPI {
+  let items: CartItem[] = [];
+
+  return {
+    get length() {
+      return items.reduce((acc, cartItem) => acc + cartItem.qty, 0);
+    },
+    get total() {
+      return items.reduce((acc, item) => acc + item.price * item.qty, 0);
+    },
+    addItem(item): CartAPI {
+      items.push(item);
+      return this;
+    },
+    add(name, price, qty = 1): CartAPI {
+      items.push({ name, price, qty });
+      return this;
+    }
+  };
+}
