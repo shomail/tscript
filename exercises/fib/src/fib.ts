ff6f750869455391b9457a00e703436aa4eedca0
Shomail <shomail1988@gmail.com>
2018-12-10 18:13:30 +0100

    fib generator

--- exercises/fib/src/fib.ts
+++ exercises/fib/src/fib.ts
@@ -1,8 +1,15 @@
-
 /**
  * Implement a generator function that can be used
  * to generate numbers in the Fibonacci Sequence
  */
-// export function* getFibSequence(): IterableIterator<number> {
- 
-// }
+export function* getFibSequence(): IterableIterator<number> {
+  let oneAgo = 0;
+  let twoAgo = 1;
+  while (true) {
+    let nextValue = twoAgo + oneAgo;
+    yield nextValue;
+    // yield last + lastlast;
+    twoAgo = oneAgo;
+    oneAgo = nextValue;
+  }
+}
